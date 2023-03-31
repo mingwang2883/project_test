85af565195fb7d7f2dde237ba0c4f44938911309
wangm <wangm@jiaketech.cn>
2023-03-31 17:13:56 +0800

    新增编译脚本

--- mp4v2-2.0.0/run_make.sh
+++ mp4v2-2.0.0/run_make.sh
@@ -3,5 +3,8 @@
 #./configure --host=mips-linux-uclibc CXXFLAGS=-fPIC CFLAGS=-fPIC CC=mips-linux-uclibc-gnu-gcc CXX=mips-linux-uclibc-gnu-g++ --prefix=$(pwd)/_install
 #./configure --host=arm-augentix-linux-uclibcgnueabihf CXXFLAGS=-fPIC CFLAGS=-fPIC CC=arm-augentix-linux-uclibcgnueabihf-gcc CXX=arm-augentix-linux-uclibcgnueabihf-g++ --prefix=$(pwd)/_install
 #./configure --host=arm-anykav500-linux-uclibcgnueabi CXXFLAGS=-fPIC CFLAGS=-fPIC CC=arm-anykav500-linux-uclibcgnueabi-gcc CXX=arm-anykav500-linux-uclibcgnueabi-g++ --prefix=$(pwd)/_install
+#./configure --host=arm-augentix-linux-uclibcgnueabihf CFLAGS=-fPIC CXXFLAGS="--std=c++98 -fPIC" CC=arm-augentix-linux-uclibcgnueabihf-gcc CXX=arm-augentix-linux-uclibcgnueabihf-g++ --prefix=$(pwd)/_install
+./configure --host=arm-anycloud-linux-uclibcgnueabi CFLAGS=-fPIC CXXFLAGS="--std=c++98 -fPIC" CC=arm-anycloud-linux-uclibcgnueabi-gcc CXX=arm-anycloud-linux-uclibcgnueabi-g++ --prefix=$(pwd)/_install
 
-./configure --host=arm-augentix-linux-uclibcgnueabihf CFLAGS=-fPIC CXXFLAGS="--std=c++98 -fPIC" CC=arm-augentix-linux-uclibcgnueabihf-gcc CXX=arm-augentix-linux-uclibcgnueabihf-g++ --prefix=$(pwd)/_install
+make -j4 && make install
+
